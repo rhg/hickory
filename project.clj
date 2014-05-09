ac2e5d03c74c97f3b146f8d4a4fec25eb8c0bc83
Ricardo Gomez <ricardo.gomez331@gmail.com>
2014-05-09 17:15:01 -0500

    fix my dep bringing clojurescript

--- project.clj
+++ project.clj
@@ -9,17 +9,17 @@
                  [org.clojure/clojurescript "0.0-1934"]
                  [quoin "0.1.0"]
                  [org.jsoup/jsoup "1.7.1"]
-                 [rhg135/node-hickory "0.0.1"]]
+                 [rhg135/node-hickory "0.0.1" :exclusions [org.clojure/clojurescript]]]
   :plugins [[codox "0.6.4"]
             [lein-cljsbuild "0.3.4"]
             [com.keminglabs/cljx "0.3.0"]
+            [org.bodil/lein-noderepl "0.1.11"]
             [com.cemerick/clojurescript.test "0.1.0"]]
   :hooks [cljx.hooks]
   :codox {:sources ["src" "target/generated-src"]
           :output-dir "codox-out"
           :src-dir-uri "http://github.com/rhg/hickory/blob/master"
           :src-linenum-anchor-prefix "L"}
-
   :cljx {:builds [{:source-paths ["src"]
                    :output-path "target/generated-src"
                    :rules :clj}
